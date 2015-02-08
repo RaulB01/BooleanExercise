ffc5a6fb14dae044e43084a66d15c1ce79e588a6
Raul Buendia <rdb24057@email.vccs.edu>
2015-02-08 17:39:20 -0500

    update

--- Assignment 1/src/Assignment1.java
+++ Assignment 1/src/Assignment1.java
@@ -1,3 +1,9 @@
+/**
+ * Raul Buendia
+ * 2/8/2015
+ * Assignment 1 - Move the first word of a user entry and move it to the back.
+ * Email:Raul.Buendia1995@gmail.com
+ */
 import java.util.Scanner;
 public class Assignment1 {
 	public static void main(String [] args) {
@@ -7,18 +13,18 @@ public class Assignment1 {
 		String sentence, text1, remaining;
 		int i;
 		
-		System.out.println("Enter a line of text:"); 
+		System.out.println("Enter a line of text:"); //asks for input
 		
-		sentence = keyboard.nextLine(); 
-		i = sentence.indexOf(" ");
-		text1 = sentence.substring(0,i);
-		remaining = sentence.substring (i + 1, sentence.length());
+		sentence = keyboard.nextLine(); // uses user input
+		i = sentence.indexOf(" "); //index of " "
+		text1 = sentence.substring(0,i); // grabs the first word
+		remaining = sentence.substring (i + 1, sentence.length()); //takes the rest of the sentence and puts it in front of first word
 
-		System.out.println("You entered: " + sentence);
+		System.out.println("You entered: " + sentence); // shows what you have entered
 	
-		System.out.println("Let me rephrase your entry:");
+		System.out.println("Let me rephrase your entry:"); 
 		
-		System.out.println(remaining + " " + text1);
+		System.out.println(remaining + " " + text1); //prints out the remaining sentence with first word in the end
 	}
 
 }
